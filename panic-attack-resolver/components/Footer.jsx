0e3f6f6d6e2d15f3038a399ca5f4e23f10411bb9
Indie Cowan <cowani@wwu.edu>
2023-11-16 14:22:24 -0800

    buttons to icons and made footer screen-size

--- panic-attack-resolver/components/Footer.jsx
+++ panic-attack-resolver/components/Footer.jsx
@@ -7,8 +7,10 @@ import {
 } from 'react-native'
 
 import {colors} from '../values/colors'
+import { Ionicons } from '@expo/vector-icons';
+import { Feather } from '@expo/vector-icons';
 
-const CustomButton = ({ title, onPress, disabled, circle, color }) => (
+const CustomIconButtonI = ({ iconName, onPress, disabled, circle, color }) => (
     <TouchableOpacity
         onPress={onPress}
         disabled={disabled}
@@ -18,49 +20,78 @@ const CustomButton = ({ title, onPress, disabled, circle, color }) => (
             { backgroundColor: color },
         ]}
     >
-        <Text style={styles.buttonText}>{title}</Text>
+        <Ionicons name={iconName} size={24} color="white" />
+    </TouchableOpacity>
+)
+
+const CustomIconButtonF = ({ iconName, onPress, disabled, circle, color }) => (
+    <TouchableOpacity
+        onPress={onPress}
+        disabled={disabled}
+        style={[
+            styles.button,
+            circle ? styles.circle : styles.squoval,
+            { backgroundColor: color },
+        ]}
+    >
+        <Feather name={iconName} size={24} color="white" />
+    </TouchableOpacity>
+)
+
+const CustomTextButton = ({ text, onPress, disabled, circle, color }) => (
+    <TouchableOpacity
+        onPress={onPress}
+        disabled={disabled}
+        style={[
+            styles.button,
+            circle ? styles.circle : styles.squoval,
+            { backgroundColor: color },
+        ]}
+    >
+        <Text style={styles.buttonText}>{text}</Text>
     </TouchableOpacity>
 )
 
 const Footer = ({ setCurrentScreen }) => (
     <View style={styles.container}>
-        <CustomButton title="Home" onPress={() => setCurrentScreen('Chat')} color={colors.defaultButtonColor} />
-        <CustomButton title="Breathing" onPress={() => setCurrentScreen('Breathing')} color={colors.defaultButtonColor} />
-        <CustomButton title="SOS" onPress={() => setCurrentScreen('SOS')} circle color={colors.sosButtonColor} />
-        <CustomButton title="Calendar" onPress={() => setCurrentScreen('Calendar')} color={colors.defaultButtonColor} />
-        <CustomButton title="More" onPress={() => setCurrentScreen('Settings')} color={colors.defaultButtonColor} />
+        <CustomIconButtonI iconName="ios-home" onPress={() => setCurrentScreen('Chat')} color={colors.defaultButtonColor} />
+        <CustomIconButtonF iconName="circle" onPress={() => setCurrentScreen('Breathing')} color={colors.defaultButtonColor} />
+        <CustomTextButton text="SOS" onPress={() => setCurrentScreen('SOS')} circle color={colors.sosButtonColor} />
+        <CustomIconButtonI iconName="calendar" onPress={() => setCurrentScreen('Calendar')} color={colors.defaultButtonColor} />
+        <CustomIconButtonI iconName="settings" onPress={() => setCurrentScreen('Settings')} color={colors.defaultButtonColor} />
     </View>
 )
 
 const styles = StyleSheet.create({
     container: {
-        flex: 1,
         flexDirection: 'row',
-        // backgroundColor: 'white',
-        justifyContent: 'center',
+        justifyContent: 'space-around', // This will distribute space around the buttons equally
+        alignItems: 'center', // This ensures all buttons are aligned in the center vertically
         paddingBottom: 10,
     },
     button: {
+        height: 70, // TODO: Adjust height to be dynamic
+        flex: 1, // This allows each button to grow and take an equal amount of space
         alignItems: 'center',
         justifyContent: 'center',
         margin: 2,
+        padding: 10, // You can adjust padding to ensure text fits well within the button
+        // Removed static width and height from here
     },
     squoval: {
         borderColor: '#332F2E',
-        width: 70,
-        height: 70,
         borderRadius: 20,
+        // Adjust padding if necessary, instead of a fixed width and height
     },
     circle: {
-        width: 70,
-        height: 70,
-        borderRadius: 35,
+        borderRadius: 35, // This will create a circle if the width and height are equal
+        // Adjust padding if necessary
     },
     buttonText: {
         fontSize: 12,
         fontWeight: 'bold',
         color: colors.appBackgroundColor,
     },
-})
+});
 
 export default Footer
