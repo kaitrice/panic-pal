01def6b3b50c95698879f13a45ad87ce938e4b1e
Indie Cowan <cowani@wwu.edu>
2023-12-02 15:29:31 -0800

    dotdotdot when waiting for bot response

--- panic-attack-resolver/components/JournalEntry.jsx
+++ panic-attack-resolver/components/JournalEntry.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { View, Text, TextInput, StyleSheet, TouchableOpacity } from 'react-native';
+import { Alert, View, Text, TextInput, StyleSheet, TouchableOpacity } from 'react-native';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import {colors} from '../values/colors'
 
